06dcbdb9c8e20347ac3da69bebf6a1b47985355b
Manuel Manriquez <manriquezmanny@gmail.com>
2023-12-10 19:16:32 -0800

    Added Remove functionality and clear all functionality

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const clearBtn = document.querySelector("#clear");
 
+// Adds li to ul
 const addItem = e => {
     e.preventDefault();
     // Saving input value in variable.
@@ -23,6 +25,7 @@ const addItem = e => {
     itemInput.value = "";
 }
 
+// Creates Button.
 function createButton(classes) {
     const button = document.createElement("button");
     button.className = classes;
@@ -31,11 +34,30 @@ function createButton(classes) {
     return button;
 }
 
+// Creates Icon
 function createIcon(classes) {
     const icon = document.createElement("i");
     icon.className = classes;
     return icon;
 }
 
+// Remove Item function using event Delegation.
+function removeItem(e) {
+    // if clicked element's parent has a class of "remove-item"
+    if (e.target.parentElement.classList.contains("remove-item")) {
+        // Traversing DOM to get to li from icon and removing it.
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+// Clears list items.
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 // Event listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
